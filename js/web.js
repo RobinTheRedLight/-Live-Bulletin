05222d058d5b1730d3874b42ac9c425589d4e506
RobinTheRedLight <robinmitra789@gmail.com>
2022-09-04 19:38:07 +0600

    DivSecFix

--- js/web.js
+++ js/web.js
@@ -31,24 +31,69 @@ const resultof = (data, catName) => {
     const inerBdy = document.getElementById('innerBody');
     inerBdy.innerHTML = ``;
     data.forEach(element => {
-        console.log(element);
         const creDiv = document.createElement('div');
         creDiv.innerHTML = `<div class="card mb-3">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="${element.image_url}" class="img-fluid rounded-start" alt="...">
+                <img src="${element.thumbnail_url}" class="img-fluid rounded-start" alt="...">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">${element.details.slice(0, 200)}...</p>
+                    <div class = "d-flex justify-content-between">
+                        <div class = "d-flex">
+                            <div class = "px-2">
+                            <img  width="35" height="35" src="${element.author.img}" class="rounded-circle" alt="image">
+                            </div>
+                            <div>
+                            ${element.author.name}
+                            </div>
+                        <div>
+                        </div>
+                        </div>
+
+                        <div>
+                        <i class="fa-regular fa-eye"></i>
+                        <div>
+                        ${element.total_view}
+                        </div>
+                        </div>
+                        <div>
+                        <button  onclick ="arrowSym('${element._id}')"   type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right"></i></button>
+                        </div>
+                    </div>
+                    <div>
+                    ${element.author.published_date}
+                    </div>
                 </div>
             </div>
         </div>
     </div>`
         inerBdy.appendChild(creDiv);
     });
+}
+const arrowSym = eleId => {
+    fetch(`https://openapi.programming-hero.com/api/news/${eleId}`)
+        .then(res => res.json())
+        .then(data => getDet(data.data))
+}
+const getDet = detDat => {
+    console.log(detDat);
+    const modalId = document.getElementById('exampleModalLabel');
+    modalId.innerText = detDat[0].title;
+    const modalIdBd = document.getElementById('modal-body');
+    modalIdBd.innerHTML = `
+    <p> ${detDat[0].details} </p>
+`
+}
+const spinnerFun = isTrue => {
+    const spinId = document.getElementById('spinner');
+    if (isTrue) {
+        spinId.classList.remove('d-none');
+    }
+    else {
+        spinId.classList.add('d-none');
+    }
 }
 dataLoad();
